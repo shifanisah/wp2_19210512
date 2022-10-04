e45014e65799deca180f4c39ead486bc65964882
shifanisah <shifahaerulnisah663@gmail.com>
2022-10-04 13:13:35 +0700

    membuat templating website sederhana

---
+++ application/views/web_template/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Shifa Hairul</li>
+ <li>Nama Belakang : Nisah</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Rengasdengklok</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Cirebon</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Catur</li>
+ </ul>
+</section>
