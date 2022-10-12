a4d578ad7d8561ffcdce576d8fe95f6fbc953086
shifanisah <shifahaerulnisah663@gmail.com>
2022-10-12 08:04:52 +0700

    tugas pertemuan 5

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -1,55 +1,67 @@
 <html>
-<head>
- <title>Form Input Matakuliah</title>
-</head>
-<body>
- <center>
- <form action="<?= base_url('matakuliah/cetak'); ?>"
-method="post">
- <table>
- <tr>
- <th colspan="3">
- Form Input Data Mata Kuliah
- </th>
- </tr>
-<tr>
- <td colspan="3">
- <hr>
- </td>
- </tr>
- <tr>
- <th>Kode MTK</th>
- <th>:</th>
- <td>
- <input type="text" name="kode" id="kode">
- </td>
- </tr>
- <tr>
- <th>Nama MTK</th>
- <td>:</td>
- <td>
- <input type="text" name="nama" id="nama">
- </td>
- </tr>
- <tr>
- <th>SKS</th>
- <td>:</td>
- <td>
- <select name="sks" id="sks">
- <option value="">Pilih SKS</option>
- <option value="2">2</option>
- <option value="3">3</option>
- <option value="4">4</option>
- </select>
- </td>
- </tr>
- <tr>
- <td colspan="3" align="center">
- <input type="submit" value="Submit">
- </td>
- </tr>
- </table>
- </form>
- </center>
-</body>
+	<head>
+		<title>Form Input Matakuliah</title>
+	</head>
+	<body>
+		<center>
+		 <form action="<?= base_url('matakuliah/cetak'); ?>"
+		 method="post">
+		 <table>
+			 <tr>
+				 <th colspan="3">
+					Form Input Data Mata Kuliah
+				 </th>
+			 </tr>
+			 <tr>
+					<th>Kode MTK</th>
+					<th>:</th>
+					<td>
+						<input type="text" name="kode" id="kode" value="<?= set_value('kode');?>">
+						
+					</td>
+			    </tr>
+			     <tr>
+					<th></th>
+					<th></th>
+					<td>
+						<font style="color:red; font-size:8; align:center;"><?= form_error('kode');?></font>
+					</td>
+			    </tr>
+			    <tr>
+					<th>Nama MTK</th>
+					<td>:</td>
+					<td>
+						<input type="text" name="nama" id="nama" value="<?= set_value('nama');?>">
+					</td>
+				</tr>
+				<tr>
+					<th></th>
+					<th></th>
+					<td>
+						<font style="color:red; font-size:8; align:center;"><?= form_error('nama');?></font>
+					</td>
+				</tr>
+				<tr>
+					<th>SKS</th>
+					<td>:</td>
+					<td>
+					 <select name="sks">
+						<option value="" <?php echo set_select('sks', '', TRUE); ?> >Pilih Sks</option>
+						<option value="2" <?php echo set_select('sks', '2'); ?> >2</option>
+						<option value="3" <?php echo set_select('sks', '3'); ?> >3</option>
+						<option value="4" <?php echo set_select('sks', '4'); ?> >4</option>
+						</select> 
+					 <font style="color:red; font-size:8; align:center;"><?= form_error('sks');?></font>
+					</td>
+			 </tr>
+			 
+			 <tr>
+				<td colspan="3" align="center">
+					<input type="submit" value="Submit">
+				</td>
+					</tr>
+				</table>
+			</form>
+		</center>
+	</body>
 </html>
